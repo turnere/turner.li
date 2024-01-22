0b8fdd96fb60a3fdba7120b794693d6d3d3051b7
Eric Turner <eric.turner.it@gmail.com>
2024-01-21 18:08:08 -0600

    built and basic information added.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# turner.li
-Resume and portfolio.
+# Eric's Main Site
+---
+
+Used for resume and portfolio. Hopefully adding blog posts and such later.
+
+
---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  const meta = {
+    title: "Eric Turner a'boot me",
+    description: "Quick portfolio and resume and someday maybe other things like a PDF generator of my resume and more!",
+    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+  };
+
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        {/* Bluseky??? <meta name="twitter:card" content="summary_large_image" /> */}
+        {/* Bluseky??? <meta name="twitter:site" content="@yourname" /> */}
+        {/* Bluseky??? <meta name="twitter:title" content={meta.title} /> */}
+        {/* Bluseky??? <meta name="twitter:description" content={meta.description} /> */}
+        {/* Bluseky??? <meta name="twitter:image" content={meta.image} /> */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
